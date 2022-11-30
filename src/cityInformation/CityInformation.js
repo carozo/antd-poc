493e5d5d414fffbb9dfa570c472bddfe6213479f
belenSuarez20 <bsuarez12052000@gmail.com>
2022-11-30 19:40:32 -0300

    add-city-forecast

--- src/cityInformation/CityInformation.js
+++ src/cityInformation/CityInformation.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Card } from 'antd';
+import moment from 'moment';
 
-export const CityInformation = () => {
+export const CityInformation = (props) => {
   const [dailyForecasts, setDailyForecasts] = useState([]);
+  const [cityName, setCityName] = useState('');
 
   const mapCityForecast = (result) => {
-    const mappedForecasts = result.DailyForecasts.map((forecast) => {
+    const mappedForecasts = result?.DailyForecasts?.map((forecast) => {
       return {
-        date: forecast.Date,
+        date: moment(forecast.Date).format("dddd, Do MMM YYYY"),
         maximum: forecast.Temperature.Maximum.Value + forecast.Temperature.Maximum.Unit,
         minimum: forecast.Temperature.Minimum.Value + forecast.Temperature.Minimum.Unit,
         day: forecast.Day.IconPhrase,
@@ -17,24 +19,41 @@ export const CityInformation = () => {
     setDailyForecasts(mappedForecasts);
   }
 
-  const getCityInfo = async () => {
+  const getCityInfo = useCallback(async (result) => {
+    const id = (result && result.length > 0) ? result[0].Key : undefined;
+    if (id) {
+      var requestOptions = {
+        method: 'GET',
+      };
+      
+      await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=qUbtmRrCaASACQZZR3RrHLqkGcUwd6Tc`, requestOptions)
+      .then(response => response.json())
+      .then(result => mapCityForecast(result))
+      .catch(error => console.log('error', error));
+    }
+  }, []);
+
+  const getCityId = useCallback(async () => {
     var requestOptions = {
       method: 'GET',
     };
 
-    await fetch("http://dataservice.accuweather.com/forecasts/v1/daily/5day/349269?apikey=qUbtmRrCaASACQZZR3RrHLqkGcUwd6Tc", requestOptions)
+    await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=qUbtmRrCaASACQZZR3RrHLqkGcUwd6Tc&q=${props.queryString}`, requestOptions)
     .then(response => response.json())
-    .then(result => mapCityForecast(result))
+    .then(result => {
+      getCityInfo(result);
+      setCityName( result && result[0].LocalizedName ? result[0]?.LocalizedName : '');
+    })
     .catch(error => console.log('error', error));
-  }
+  }, [getCityInfo, props.queryString]);
 
   useEffect(() => {
-    getCityInfo();
-  }, [])
+    getCityId();
+  }, [props.queryString, getCityId])
 
   return (
     <div>
-      <label style={{ fontSize: '30px' }}>Montevideo</label>
+      <label style={{ fontSize: '30px' }}>{cityName}</label>
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '10px' }}>
         {dailyForecasts.map((forecast) => (
           <Card title={forecast.date} style={{ width: 300 }} key={forecast.date}>
--- src/homeLayout/HomeLayout.tsx
+++ src/homeLayout/HomeLayout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { AutoComplete, Input, MenuProps } from 'antd';
+import { AutoComplete, Button, Input, MenuProps, Space } from 'antd';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import { CityInformation } from '../cityInformation/CityInformation';
 
@@ -24,42 +24,53 @@ const sideBarOptions: MenuProps['items'] = [
   },
 ];
 
-export const HomeLayout: React.FC = () => (
-  <Layout style={{ height: '100vh' }}>
-    <Header className="header">
-      Cities
-      <AutoComplete
-        dropdownMatchSelectWidth={252}
-        style={{ width: 300 }}
-        options={[]}
-        onSelect={() => {}}
-        onSearch={() => {}}
-      >
-        <Input.Search size="large" placeholder="input here" enterButton />
-      </AutoComplete>    
-    </Header>
-    <Content style={{ padding: '0 30px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>City Name</Breadcrumb.Item>
-      </Breadcrumb>
-      <Layout className="site-layout-background" style={{ padding: '24px 0', height: '95%' }}>
-        <Sider className="site-layout-background" width={200}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['sub1']}
-            style={{ height: '100%' }}
-            items={sideBarOptions}
-          />
-        </Sider>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
-          <CityInformation />
-        </Content>
-      </Layout>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>©2020 Created by BCS</Footer>
-  </Layout>
-);
+export const HomeLayout = () => {
+  const [selectedCity, setSelectedCity] = useState('');
+
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Header className="header">
+        Cities
+        <AutoComplete
+          dropdownMatchSelectWidth={252}
+          style={{ width: 300 }}
+          options={[]} //Aca irian todas las ciudades disponibles, o ponemos todas?
+          onSelect={() => {}}
+          onSearch={(value) => setSelectedCity(value)}
+        >
+          <Input.Search size="large" placeholder="input here" enterButton />
+        </AutoComplete>    
+      </Header>
+      <Content style={{ padding: '0 30px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>City Name</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0', height: '95%' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['sub1']}
+              style={{ height: '100%' }}
+              items={sideBarOptions}
+            />
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Space wrap style={{ marginBottom: '20px' }}>
+              <Button type="primary" onClick={() => setSelectedCity('Montevideo')}>Montevideo</Button>
+              <Button type="primary" onClick={() => setSelectedCity('Madrid')}>Madrid</Button>
+              <Button type="primary" onClick={() => setSelectedCity('Roma')}>Roma</Button>
+              <Button type="primary" onClick={() => setSelectedCity('Amsterdam')}>Amsterdam</Button>
+              <Button type="primary" onClick={() => setSelectedCity('Nueva York')}>Nueva York</Button>
+            </Space>
+            <CityInformation queryString={selectedCity} />
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>©2020 Created by BCS</Footer>
+    </Layout>
+  );
+};
 
 export default HomeLayout;
